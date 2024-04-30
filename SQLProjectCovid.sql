fe9026b13a2f145c3a98f3bdd71d923497a7520a
ann-e888 <132256907+ann-e888@users.noreply.github.com>
2024-04-30 19:14:10 +0200

    Update on original code form the online course to fit new database.

--- SQLProjectCovid.sql
+++ SQLProjectCovid.sql
@@ -1,19 +1,19 @@
-SELECT * FROM CovidDeaths$
-ORDER BY 3,4
+SELECT * FROM CovidDeaths
+ORDER BY 3,4 
 
 --SELECT * FROM CovidVaccinations$
 --ORDER BY 3,4
 
 --Selecting data to use
 SELECT location, date, total_cases, new_cases, total_deaths, population
-FROM CovidDeaths$
+FROM CovidDeaths
 WHERE continent IS NOT NULL
 ORDER BY 1,2
 
 
 --Loking at total_cases vs total_deaths, likelihood of dying
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as death_percentage
-FROM CovidDeaths$
+FROM CovidDeaths
 WHERE location LIKE '%states%'
 AND continent IS NOT NULL
 ORDER BY 2
@@ -21,29 +21,28 @@ ORDER BY 2
 
 --Looking at total cases vs population
 SELECT location, date, total_cases, population, (total_cases/population)*100 as infection_percentage
-FROM CovidDeaths$
+FROM CovidDeaths
 WHERE location LIKE '%poland%'
 AND continent IS NOT NULL
 ORDER BY 2
 
 --Looking at countries with highest infection rate vs their population
 SELECT location, population, MAX(total_cases) as infection_count, MAX((total_cases/population)*100) as infection_percentage
-FROM CovidDeaths$
+FROM CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY population, location
 ORDER BY 4 DESC
 
 --Countries with highest death count per population
-SELECT location, MAX(CAST(total_deaths AS INT)) as death_count
-FROM CovidDeaths$
+SELECT location, MAX(total_deaths) as death_count
+FROM CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY location
 ORDER BY 2 DESC
 
-
 --Looking at countires with highest death rate
 SELECT location, MAX(CAST(total_deaths AS INT))/MAX(total_cases) as death_rate
-FROM CovidDeaths$
+FROM CovidDeaths
 WHERE continent IS NOT NULL
 GROUP BY  location
 ORDER BY 2 DESC
@@ -51,18 +50,21 @@ ORDER BY 2 DESC
 
 --By continent
 SELECT location, MAX(CAST(total_deaths AS INT)) as death_count
-FROM CovidDeaths$
-WHERE continent IS NULL
+FROM CovidDeaths
+WHERE location IN ('Europe', 'Africa', 'North America', 'Asia', 'Australia', 'South America')
 GROUP BY  location
 ORDER BY 2 DESC
 
 --Global numbers of death percentage
-SELECT date, SUM(new_cases) cases, SUM(CAST(new_deaths AS INT)) deaths,
-SUM(CAST(new_deaths AS INT))/SUM(new_cases)*100 as death_percentage
-FROM CovidDeaths$
-WHERE continent IS NOT NULL
+SELECT date, SUM(new_cases) cases, SUM(new_deaths) deaths,
+    CASE 
+        WHEN SUM(new_cases) = 0 THEN 0 -- Avoid division by zero
+        ELSE SUM(CAST(new_deaths AS INT)) / SUM(new_cases) * 100 
+    END AS death_percentage
+FROM CovidDeaths
+WHERE location = 'World'
 GROUP BY date
-ORDER BY 4 DESC
+ORDER BY death_percentage DESC;
 
 
 --Population vs vaccination, CTE
@@ -71,9 +73,9 @@ continent, location, date, population, new_vacconations, rolling_count_vaccinati
 as(
 SELECT cd.continent, cd.location, cd.date, cd.population,
 		cv.new_vaccinations, 
-		SUM(CAST(cv.new_vaccinations AS INT)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as rolling_count_vaccinations
-FROM CovidDeaths$ cd
-JOIN CovidVaccinations$ cv
+		SUM(cv.new_vaccinations) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as rolling_count_vaccinations
+FROM CovidDeaths cd
+JOIN CovidVaccinations cv
 	 ON cd.location = cv.location
 	 AND cd.date = cv.date
 WHERE cd.continent IS NOT NULL
@@ -97,9 +99,9 @@ rolling_count_vaccinations numeric)
 INSERT INTO #PopulationVaccinatedPercent
 SELECT cd.continent, cd.location, cd.date, cd.population,
 		cv.new_vaccinations, 
-		SUM(CAST(cv.new_vaccinations AS INT)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as rolling_count_vaccinations
-FROM CovidDeaths$ cd
-JOIN CovidVaccinations$ cv
+		SUM(cv.new_vaccinations) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as rolling_count_vaccinations
+FROM CovidDeaths cd
+JOIN CovidVaccinations cv
 	 ON cd.location = cv.location
 	 AND cd.date = cv.date
 WHERE cd.continent IS NOT NULL
@@ -114,10 +116,13 @@ ORDER BY 2,3
 CREATE VIEW PopulationVaccinatedPercent AS
 SELECT cd.continent, cd.location, cd.date, cd.population,
 		cv.new_vaccinations, 
-		SUM(CAST(cv.new_vaccinations AS INT)) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as rolling_count_vaccinations
-FROM CovidDeaths$ cd
-JOIN CovidVaccinations$ cv
+		SUM(cv.new_vaccinations) OVER (PARTITION BY cd.location ORDER BY cd.location, cd.date) as rolling_count_vaccinations
+FROM CovidDeaths cd
+JOIN CovidVaccinations cv
 	 ON cd.location = cv.location
 	 AND cd.date = cv.date
 WHERE cd.continent IS NOT NULL
 AND cv.continent IS NOT NULL
+
+
+
